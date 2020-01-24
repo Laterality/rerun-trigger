e86467c4fa6ea86f9883828e1133e49630855ecb
Jinwoo Shin <bluesky0255@gmail.com>
2020-01-24 17:04:07 +0900

    feat: Take configuration from yaml file

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Port int `yaml:"port"`
+	}
+	Spec struct {
+		ImageName     string `yaml:"image"`
+		ContainerName string `yaml:"containerName"`
+	} `yaml:"spec"`
+}
+
+func NewConfigFromFile(path string) (*Config, error) {
+	var err error = nil
+	config := new(Config)
+	bytes, err := ioutil.ReadFile(path)
+	log.Println("read: ", string(bytes))
+	if err != nil {
+		return nil, err
+	}
+	yaml.Unmarshal(bytes, config)
+
+	return config, err
+}
---
+++ config/config_test.go
@@ -0,0 +1,23 @@
+package config
+
+import "testing"
+
+func Test_read_spec_from_file(t *testing.T) {
+	expectedImageName := "nginx"
+	expectedContainerName := "webserver"
+	conf, _ := NewConfigFromFile("./config_example.yml")
+	if conf.Spec.ImageName != expectedImageName {
+		t.Errorf("Image name not matched. expected '%s' but actual was '%s'\n", expectedImageName, conf.Spec.ImageName)
+	}
+	if conf.Spec.ContainerName != expectedContainerName {
+		t.Errorf("Container name not matched. expected '%s' but actual was '%s'\n", expectedContainerName, conf.Spec.ContainerName)
+	}
+}
+
+func Test_read_server_from_file(t *testing.T) {
+	expectedPort := 8000
+	conf, _ := NewConfigFromFile("./config_example.yml")
+	if conf.Server.Port != expectedPort {
+		t.Errorf("Server port not matched. expected %d but actual was '%d'\n", expectedPort, conf.Server.Port)
+	}
+}
